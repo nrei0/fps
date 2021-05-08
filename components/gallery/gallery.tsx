c84a469d2ac62080b9415d1b861fe41a15ea5e69
Andrei Tiurin <nrei.tiurin@gmail.com>
2021-05-08 16:29:20 +0300

    Implement Gallery & photo cards display in stylized layout

--- components/gallery/gallery.tsx
+++ components/gallery/gallery.tsx
@@ -1,33 +1,48 @@
 import axios, { AxiosResponse } from 'axios'
+import cn from 'classnames'
 import { useQuery } from 'react-query'
 import { Spinner } from '../spinner'
 import { FlickrApiResponse } from '../../types/flickr'
 import { PhotoCard } from '../photo-card'
+import styles from './gallery.module.scss'
 
-export const Gallery: React.FC = () => {
+interface Props {
+  className?: string
+}
+
+export const Gallery: React.FC<Props> = ({ className }) => {
   const { isLoading, data } = useQuery(
     'public_photos',
     () =>
       axios.get<unknown, AxiosResponse<FlickrApiResponse>>('/api/flickr').then((res) => res.data),
     { refetchOnReconnect: false, refetchOnWindowFocus: false }
   )
+  const numOfItems = data?.length
+  const emptyCols = 3 - (numOfItems % 3)
 
   return isLoading ? (
     <Spinner alt="Gallery is loading" />
   ) : (
-    <div>
-      {data.map(({ title, description, author, authorLink, link, tags, imageUrl }) => (
-        <PhotoCard
-          key={link}
-          title={title}
-          description={description}
-          imageUrl={imageUrl}
-          author={author}
-          authorLink={authorLink}
-          link={link}
-          tags={tags}
-        />
-      ))}
+    <div className={cn(className, styles.gallery)}>
+      <ul className={styles.list}>
+        {data.map(({ title, description, author, authorLink, link, tags, imageUrl }) => (
+          <li className={styles.item} key={link}>
+            <PhotoCard
+              className={styles.card}
+              title={title}
+              description={description}
+              imageUrl={imageUrl}
+              author={author}
+              authorLink={authorLink}
+              link={link}
+              tags={tags}
+            />
+          </li>
+        ))}
+        {new Array(emptyCols).fill(null).map((_, idx) => (
+          <li key={idx} className={styles.item}></li>
+        ))}
+      </ul>
     </div>
   )
 }
--- components/photo-card/photo-card.tsx
+++ components/photo-card/photo-card.tsx
@@ -1,8 +1,10 @@
 import Image from 'next/image'
+import cn from 'classnames'
 import styles from './photo-card.module.scss'
 
 interface Props {
-  title: string
+  className?: string
+  title?: string
   link: string
   imageUrl: string
   tags: string
@@ -19,35 +21,46 @@ export const PhotoCard: React.FC<Props> = ({
   description,
   author,
   authorLink,
+  className,
 }) => {
   return (
-    <div key={link}>
-      <div style={{ width: '200px', height: '200px', position: 'relative' }}>
-        <Image
-          className={styles.image}
-          src={imageUrl}
-          alt={title}
-          layout="fill"
-          quality="100"
-          objectFit="contain"
-        />
+    <div className={cn(className, styles.card)} key={link}>
+      <div className={styles['image-wrapper']}>
+        <a href={link} title={title}>
+          <Image
+            className={styles.image}
+            src={imageUrl}
+            alt={title}
+            layout="fill"
+            quality="100"
+            objectFit="cover"
+          />
+        </a>
       </div>
-      <div>
+      <h3 className={styles.title}>
         {title && (
-          <a href={link} title="">
+          <a href={link} title={title}>
             {title}
           </a>
         )}
-      </div>
-      <div>
+      </h3>
+      <p className={styles.author}>
         by{' '}
-        <a href={authorLink} title={`Visit "${author}" profile`}>
+        <a className={styles['author-link']} href={authorLink} title={`Visit "${author}" profile`}>
           {author}
         </a>
+      </p>
+      <p className={styles.description} dangerouslySetInnerHTML={{ __html: description }} />
+      <div>
+        <h5 className={styles['tags-title']}>Tags</h5>
+        <ul className={styles.tags}>
+          {tags.split(' ').map((item) => (
+            <li className={styles.tag} key={item}>
+              <a href={`https://www.flickr.com/photos/tags/${item}`}>{item}</a>
+            </li>
+          ))}
+        </ul>
       </div>
-      <div dangerouslySetInnerHTML={{ __html: description }} />
-      <div>Tags: {tags}</div>
-      <hr style={{ margin: '32px 0' }} />
     </div>
   )
 }
