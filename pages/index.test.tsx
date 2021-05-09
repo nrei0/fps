fc1d66c33ca9e50b93543ca0c1b8f1940a7d7eff
Andrei Tiurin <nrei.tiurin@gmail.com>
2021-05-09 18:42:48 +0300

    Update snapshot test

--- pages/index.test.tsx
+++ pages/index.test.tsx
@@ -1,6 +1,11 @@
 import renderer from 'react-test-renderer'
 import Home from './index'
 
+jest.mock('react-query', () => ({
+  ...jest.requireActual('react-query'),
+  useQuery: () => ({ isLoading: true, data: undefined }),
+}))
+
 describe('Home page', () => {
   it('should match the snapshot', () => {
     const tree = renderer.create(<Home />).toJSON()
